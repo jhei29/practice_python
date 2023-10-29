5e35c9eeecc96f76781949d3cf9adc20413168dd
Jerome <100307631+jhei29@users.noreply.github.com>
2023-10-29 15:21:36 +0800

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,97 +1,87 @@
+import secrets
 import MySQLdb
 import logging
-from connect import Connect
-from validation import IntegerInputValidator, MinimumLengthValidator
+import importlib
+from datetime import datetime
+from exception import InputError
+from database.connect import Connect
+from config import PASSWORD_HASHER, DEV_MODE
+from validation import MIN_EMAIL_LENGTH, PasswordValidator, MinimumLengthValidator, UsernameValidator
 
 logger = logging.getLogger(__name__)
 
 
-class Provider(object):
-    """Provider CRUD operations"""
+class Register:
+    """User Registration"""
 
-    def __init__(self):
-        self.int_validator = IntegerInputValidator()
+    def __init__(self, data, *args, **kwargs):
+        self.data = data
+        self.password_validator = PasswordValidator()
+        self.username_validator = UsernameValidator()
         self.min_validator = MinimumLengthValidator()
+        # load password hasher class
+        hasher = getattr(importlib.import_module('hashers'), PASSWORD_HASHER)
+        self.password_hasher = hasher()
+        self.salt = secrets.token_hex(32).encode()
 
-    def insert(self, providers_list):
-        try:
-            conn = Connect()
-            cursor = conn.db.cursor()
-            cursor.executemany(
-                """
-                INSERT INTO providers (name, address, phone)
-                    VALUES (%(name)s, %(address)s, %(phone)s)
-                """, providers_list
-            )
-            conn.db.commit()
-            logger.info('Provider %02d inserted' % cursor.lastrowid)
-        except MySQLdb.MySQLError as e:
-            logger.error('Could not insert provider: %s' % e)
-        finally:
-            conn.db.close()
+    def fields_validation(self, *args, **kwargs):
+        username = self.data.get('username').strip()
+        self.username_validator.validate(username)
 
-    def retrieve(self, provider_id):
-        result = None
-        try:
-            conn = Connect()
-            cursor = conn.db.cursor()
-            cursor.execute(
-                """
-                SELECT * FROM providers WHERE providerID=%(id)s
-                """, {'id': provider_id}
-            )
-            result = cursor.fetchall()
-        except MySQLdb.MySQLError as e:
-            logger.error('Could not retrieve the records: %s' % e)
-        finally:
-            conn.db.close()
-        return result
+        password1 = self.data.get('password1')
+        password2 = self.data.get('password2')
+        if password1 != password2:
+            raise InputError(
+                "The two password fields didn't match.", 'password_mismatch')
+        self.password_validator.validate(password1)
 
-    def update(self, **kwargs):
-        self.min_validator.validate(kwargs.get('name'), 3)
-        self.int_validator.validate(kwargs.get('id'))
-        try:
-            conn = Connect()
-            result = conn.db.cursor().execute(
-                """
-                UPDATE providers
-                    SET name=%(name)s, address=%(address)s, phone=%(phone)s
-                WHERE providerID=%(id)s AND EXISTS (
-                    SELECT providerID FROM (
-                        SELECT providerID FROM providers WHERE providerID=%(id)s
-                    ) AS tmp
+        email = self.data.get('email')
+        email_confirm = self.data.get('email_confirm')
+        if email != email_confirm:
+            raise InputError(
+                'Both email addresses fields must match.', 'email_mismatch')
+        self.min_validator.validate(email, MIN_EMAIL_LENGTH)
+
+    def create(self):
+        # validate
+        self.fields_validation()
+        # password validation
+        password = self.data.get('password1')
+
+        encoded_password = self.password_hasher.encode(
+            password)
+
+        new_user = {
+            'username': self.data.get('username'),
+            'password': encoded_password,
+            'email': self.data.get('email'),
+            'date_joined': datetime.utcnow().isoformat()
+        }
+
+        return new_user
+
+    def save(self):
+        user = self.create()
+        if user:
+            try:
+                conn = Connect()
+                cursor = conn.db.cursor()
+                cursor.execute(
+                    """
+                    INSERT INTO user (username, password, email, date_joined)
+                        VALUES (%(username)s, %(password)s,
+                                %(email)s, %(date_joined)s)
+                    """, user
                 )
-                """, kwargs
-            )
-            conn.db.commit()
+                conn.db.commit()
+                logger.info('User %02d inserted' % cursor.lastrowid)
 
-            if result:
-                logger.info('Provider id %02d updated' % kwargs.get('id'))
-            else:
-                logger.info('Could not update provider')
-        except MySQLdb.MySQLError as e:
-            logger.error('Could not update provider: %s' % e)
-        finally:
-            conn.db.close()
+                if DEV_MODE:
+                    logger.debug(
+                        '%(username)s, %(password)s, '
+                        '%(email)s, %(date_joined)s' % user)
 
-    def delete(self, provider_id):
-        try:
-            conn = Connect()
-            result = conn.db.cursor().execute(
-                """
-                DELETE FROM providers WHERE providerID=%(id)s AND EXISTS (
-                    SELECT providerID FROM (
-                        SELECT providerID FROM providers WHERE providerID=%(id)s
-                    ) AS tmp
-                )
-                """, {'id': provider_id}
-            )
-            conn.db.commit()
-            if result:
-                logger.warning('provider id %02d deleted' % provider_id)
-            else:
-                logger.warning('Provider does not exist')
-        except MySQLdb.MySQLError as e:
-            logger.error('Could not delete provider: %s' % e)
-        finally:
-            conn.db.close()
+            except MySQLdb.MySQLError as e:
+                logger.error('Could not insert provider: %s' % e)
+            finally:
+                conn.db.close()
