4ea56f1b18cf88ab33309e68b478ef7d2eb27e30
Jerome <100307631+jhei29@users.noreply.github.com>
2023-10-29 15:46:41 +0800

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,76 +1,85 @@
-import argon2
-import bcrypt
-import hashlib
-import binascii
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-class Argon2PasswordHasher:
-    """
-    Secure password hashing using the argon2 algorithm.
+class UpdateSerializer:
 
-    This is the winner of the Password Hashing Competition 2013-2015
-    (https://password-hashing.net). It requires the argon2-cffi library which
-    depends on native C code and might cause portability issues.
-    """
-    time_cost = 2
-    memory_cost = 512
-    parallelism = 2
+    fields = None
 
-    rounds = 12
+    def serialize(self, data):
 
-    def salt(self):
-        return bcrypt.gensalt(self.rounds).decode()
+        result = ''
 
-    def encode(self, password, salt):
-        data = argon2.low_level.hash_secret(
-            password.encode(),
-            salt.encode(),
-            time_cost=self.time_cost,
-            memory_cost=self.memory_cost,
-            parallelism=self.parallelism,
-            hash_len=argon2.DEFAULT_HASH_LENGTH,
-            type=argon2.low_level.Type.I,
-        )
-        return data.decode('ascii')
+        keys = list(data.keys())
+        if keys:
+            key = keys[0]
+            if data.get(key):
+                result += f'{key}=%({key})s'
+            for key in keys[1:]:
+                if data.get(key):
+                    result += f', {key}=%({key})s'
 
-    def verify(self, password, encoded):
-        try:
-            return argon2.low_level.verify_secret(
-                encoded.encode('ascii'),
-                password.encode(),
-                type=argon2.low_level.Type.I,
-            )
-        except argon2.exceptions.VerificationError:
-            return False
+        return result
+
+    def updatefields(self, data):
+
+        if not (data and isinstance(data, dict)):
+            logger.error('SerializerError: data is not valid')
+            return None
+
+        return self.serialize(data)
 
 
-class BCryptSHA256PasswordHasher:
-    """
-    Secure password hashing using the bcrypt algorithm (recommended)
+class UserUpdateSerializer(UpdateSerializer):
 
-    This is considered by many to be the most secure algorithm but you
-    must first install the bcrypt library.  Please be warned that
-    this library depends on native C code and might cause portability
-    issues.
-    """
-    digest = hashlib.sha256
-    rounds = 12
+    fields = ['first_name', 'last_name', 'email']
 
-    def salt(self):
-        return bcrypt.gensalt(self.rounds)
 
-    def encode(self, password, salt):
-        password = password.encode()
-        if self.digest is not None:
-            password = binascii.hexlify(self.digest(password).digest())
+class ClientSerializer(UpdateSerializer):
 
-        data = bcrypt.hashpw(password, salt)
-        return data.decode('ascii')
+    fields = ['first_name', 'last_name', 'email', 'phone']
 
-    def verify(self, password, encoded):
-        try:
-            password = binascii.hexlify(
-                self.digest(password.encode()).digest())
-            return bcrypt.checkpw(password, encoded.encode('ascii'))
-        except Exception:
-            return False
+
+class InsertSerializer:
+
+    fields = None
+
+    def serialize(self, data):
+
+        columns, values = '', ''
+
+        if self.fields:
+            field = self.fields[0]
+            if data.get(field):
+                columns += f'{field}'
+                values += f'%({field})s'
+            for field in self.fields[1:]:
+                if data.get(field):
+                    columns += f', {field}'
+                    values += f', %({field})s'
+
+        return columns, values
+
+    def insertfields(self, data):
+
+        if not (data and isinstance(data, dict)):
+            logger.error('SerializerError: data is not valid')
+            return None
+
+        return self.serialize(data)
+
+
+class UserInsertSerializer(InsertSerializer):
+
+    fields = ['username', 'password', 'email']
+
+
+class AppointmentSerializer(InsertSerializer):
+
+    fields = ['pet_id', 'reason', 'datetime', 'description', 'doctor_id']
+
+
+class AddressSerializer(InsertSerializer):
+
+    fields = ['line_1', 'line_2', 'city', 'state']
