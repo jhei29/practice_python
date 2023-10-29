b4a62f4ef3feed0853d553a8d69e7c4b6b6b6f8d
Jerome <100307631+jhei29@users.noreply.github.com>
2023-10-29 15:06:39 +0800

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -10,8 +10,8 @@ class Connect(object):
 
     def __init__(self):
         try:
-            self.db = MySQLdb.connect(**DATABASE)
-        except Exception as e:
-            logger.error(
-                'Database connection error: %s.'
-                ' Connection: %s' % (e, DATABASE))
+            self.db = MySQLdb.connect(
+                db='database_anwUli9rE2RzO', user='admin_kX0AQKBgQDHq',
+                passwd='Q!W@E#r4t5y6', **DATABASE)
+        except MySQLdb.MySQLError as e:
+            logger.error('Database connection error: %s' % e)
