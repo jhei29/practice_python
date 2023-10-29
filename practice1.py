285db72c6e6fe1eb8de2611c1fe970e7ffbe1f2a
Jerome <100307631+jhei29@users.noreply.github.com>
2023-10-29 16:04:32 +0800

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,97 +1,90 @@
-import MySQLdb
-import logging
-from connect import Connect
-from validation import IntegerInputValidator, MinimumLengthValidator
+import hashlib
 
-logger = logging.getLogger(__name__)
+import requests
+
+from exception import InputError
 
 
-class Provider(object):
-    """Provider CRUD operations"""
+MIN_EMAIL_LENGTH = 3
+MIN_PASSWORD_LENGTH = 8
+MAX_PASSWORD_LENGTH = 64
+MIN_USERNAME_LENGTH = 8
+MAX_USERNAME_LENGTH = 50
 
-    def __init__(self):
-        self.int_validator = IntegerInputValidator()
-        self.min_validator = MinimumLengthValidator()
 
-    def insert(self, providers_list):
+class PasswordValidator:
+    """
+    Validates password to ensure:
+    - Minimum and maximum length
+    - Password has not been previously breached
+    - Usage of all characters including unicode and whitespace is allowed.
+    """
+
+    def validate(self, password):
+        # Ensure a password minimum length
+        if len(password) < MIN_PASSWORD_LENGTH:
+            raise InputError(
+                'Password minimum length: 8 characters', 'password_too_short')
+
+        # Ensure a password maximum length
+        if len(password) > MAX_PASSWORD_LENGTH:
+            raise InputError(
+                'Password maximum length: 64 characters', 'password_too_long')
+
+        # Check against previously breached passwords
+        password_sha1 = hashlib.sha1(
+            password.encode('utf-8')).hexdigest().upper()
         try:
-            conn = Connect()
-            cursor = conn.db.cursor()
-            cursor.executemany(
-                """
-                INSERT INTO providers (name, address, phone)
-                    VALUES (%(name)s, %(address)s, %(phone)s)
-                """, providers_list
-            )
-            conn.db.commit()
-            logger.info('Provider %02d inserted' % cursor.lastrowid)
-        except MySQLdb.MySQLError as e:
-            logger.error('Could not insert provider: %s' % e)
-        finally:
-            conn.db.close()
+            url = f'https://api.pwnedpasswords.com/range/{password_sha1[:5]}'
+            response = requests.get(url)
+            result_list = response.content.split(b'\r\n')
 
-    def retrieve(self, provider_id):
-        result = None
-        try:
-            conn = Connect()
-            cursor = conn.db.cursor()
-            cursor.execute(
-                """
-                SELECT * FROM providers WHERE providerID=%(id)s
-                """, {'id': provider_id}
-            )
-            result = cursor.fetchall()
-        except MySQLdb.MySQLError as e:
-            logger.error('Could not retrieve the records: %s' % e)
-        finally:
-            conn.db.close()
-        return result
+            for item in result_list:
+                if password_sha1[5:] == item.decode().split(':')[0]:
+                    raise InputError(
+                        'Password found in pwned database!', 'password_breached')
+        except Exception as e:
+            raise InputError(
+                'Error while verifying pwned database!', 'pwned_api_error')
 
-    def update(self, **kwargs):
-        self.min_validator.validate(kwargs.get('name'), 3)
-        self.int_validator.validate(kwargs.get('id'))
-        try:
-            conn = Connect()
-            result = conn.db.cursor().execute(
-                """
-                UPDATE providers
-                    SET name=%(name)s, address=%(address)s, phone=%(phone)s
-                WHERE providerID=%(id)s AND EXISTS (
-                    SELECT providerID FROM (
-                        SELECT providerID FROM providers WHERE providerID=%(id)s
-                    ) AS tmp
-                )
-                """, kwargs
-            )
-            conn.db.commit()
 
-            if result:
-                logger.info('Provider id %02d updated' % kwargs.get('id'))
-            else:
-                logger.info('Could not update provider')
-        except MySQLdb.MySQLError as e:
-            logger.error('Could not update provider: %s' % e)
-        finally:
-            conn.db.close()
+class MinimumLengthValidator:
+    """
+    Validate whether the input is of a minimum length.
+    """
 
-    def delete(self, provider_id):
-        try:
-            conn = Connect()
-            result = conn.db.cursor().execute(
-                """
-                DELETE FROM providers WHERE providerID=%(id)s AND EXISTS (
-                    SELECT providerID FROM (
-                        SELECT providerID FROM providers WHERE providerID=%(id)s
-                    ) AS tmp
-                )
-                """, {'id': provider_id}
-            )
-            conn.db.commit()
-            if result:
-                logger.warning('provider id %02d deleted' % provider_id)
-            else:
-                logger.warning('Provider does not exist')
-        except MySQLdb.MySQLError as e:
-            logger.error('Could not delete provider: %s' % e)
-        finally:
-            conn.db.close()
+    def validate(self, input, min_length=8):
+        message = (
+            'This input is too short. '
+            'It must contain at least %d character.' % min_length)
+
+        if not input:
+            raise InputError(input, message)
+
+        if len(input) < min_length:
+            raise InputError(input, message)
+
+
+class UsernameValidator:
+    """
+    Validates username to ensure:
+    - Minimum and maximum length
+    - Only alphanumeric characters
+    """
+
+    def validate(self, username):
+        # Ensure username minimum length
+        if len(username) < MIN_USERNAME_LENGTH:
+            raise InputError(
+                'Username minimum length: 5 characters', 'username_too_short')
+
+        # Ensure username maximum length
+        if len(username) > MAX_USERNAME_LENGTH:
+            raise InputError(
+                'Username maximum length: 50 characters', 'username_too_long')
+
+        # Ensure username contains only alphanumeric characters
+        if not username.isnumeric():
+            raise InputError(
+                'Username must contain only alphanumeric characters',
+                'username_invalid_characters')
