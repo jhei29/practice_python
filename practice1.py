231bd867da59de5eba96a0caa7ae0502c4894607
Jerome <100307631+jhei29@users.noreply.github.com>
2023-10-29 15:42:31 +0800

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,85 +1,89 @@
 import logging
+import importlib
+from datetime import datetime
+from MySQLdb import MySQLError
+from config import PASSWORD_HASHER
+from database.connect import Connect
+
+from decorators import is_authorized
 
 logger = logging.getLogger(__name__)
 
 
-class UpdateSerializer:
+class Auth:
+    """User Authentication"""
 
-    fields = None
+    def __init__(self):
 
-    def serialize(self, data):
+        self.id = None
+        self.is_authenticate = False
+        # load password hasher class from config file
+        self.hasher = getattr(
+            importlib.import_module('hashers'), PASSWORD_HASHER)()
 
-        result = ''
+    def login(self, username, password):
 
-        if self.fields:
-            field = self.fields[0]
-            if data.get(field):
-                result += f'{field}=%({field})s'
-            for field in self.fields[1:]:
-                if data.get(field):
-                    result += f', {field}=%({field})s'
+        conn = Connect()
+        try:
+            cursor = conn.db.cursor()
+            cursor.execute(
+                """
+                SELECT id, password FROM users
+                WHERE username=%(username)s
+                """, {'username': username}
+            )
+            result = cursor.fetchone()
 
+            if not (result and self.hasher.verify(password, result[1])):
+                raise MySQLError
+
+            self.is_authenticate = True
+            self.id = result[0]
+
+            cursor.execute(
+                """
+                UPDATE users
+                SET last_login=%(last_login)s
+                WHERE id=%(id)s
+                """,
+                {
+                    'last_login': datetime.now(),
+                    'id': self.id
+                }
+            )
+            conn.db.commit()
+            result = cursor.rowcount
+
+            if not result:
+                logger.error('Last login could not be updated')
+
+        except Exception:
+            logger.error('Login authorization failure')
+        finally:
+            conn.db.close()
+
+    def logout(self):
+
+        self.id = None
+        self.is_authenticate = False
+
+    @is_authorized
+    def roles(self):
+
+        result = None
+        conn = Connect()
+        try:
+            cursor = conn.db.cursor()
+            cursor.execute(
+                """
+                SELECT name FROM roles
+                INNER JOIN user_roles ON roles.id = user_roles.role_id
+                WHERE user_roles.user_id = %(user_id)s
+                """, {'user_id': self.id}
+            )
+            result = [role[0] for role in cursor.fetchall()]
+        except MySQLError:
+            logger.error('Could not retrieve the user record')
+        finally:
+            conn.db.close()
         return result
-
-    def updatefields(self, data):
-
-        if not (data and isinstance(data, dict)):
-            logger.error('SerializerError: data is not valid')
-            return None
-
-        return self.serialize(data)
-
-
-class UserUpdateSerializer(UpdateSerializer):
-
-    fields = ['first_name', 'last_name', 'email']
-
-
-class ClientSerializer(UpdateSerializer):
-
-    fields = ['id', 'uuid', 'first_name', 'last_name', 'email',
-              'phone', 'address_id', 'created_at']
-
-
-class InsertSerializer:
-
-    fields = None
-
-    def serialize(self, data):
-
-        columns, values = '', ''
-
-        if self.fields:
-            field = self.fields[0]
-            if data.get(field):
-                columns += f'{field}'
-                values += f'%({field})s'
-            for field in self.fields[1:]:
-                if data.get(field):
-                    columns += f', {field}'
-                    values += f', %({field})s'
-
-        return columns, values
-
-    def insertfields(self, data):
-
-        if not (data and isinstance(data, dict)):
-            logger.error('SerializerError: data is not valid')
-            return None
-
-        return self.serialize(data)
-
-
-class UserInsertSerializer(InsertSerializer):
-
-    fields = ['username', 'password', 'email']
-
-
-class AppointmentSerializer(InsertSerializer):
-
-    fields = ['pet_id', 'reason', 'datetime', 'description', 'doctor_id']
-
-
-class AddressSerializer(InsertSerializer):
-
-    fields = ['line_1', 'line_2', 'city', 'state']
