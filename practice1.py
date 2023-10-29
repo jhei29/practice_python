906a16e629ed33631ed8cb68dfe0c2fb6adfb129
Jerome <100307631+jhei29@users.noreply.github.com>
2023-10-29 15:02:48 +0800

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,89 +1,58 @@
-import secrets
-import MySQLdb
-import logging
-import importlib
-from datetime import datetime
-from exception import InputError
-from database.connect import Connect
-from config import PASSWORD_HASHER, DEV_MODE
-from validation import MIN_EMAIL_LENGTH, PasswordValidator, MinimumLengthValidator, UsernameValidator
+import os
 
-logger = logging.getLogger(__name__)
+BASE_DIR = os.environ.get('BASE_DIR')
+
+PROFILES_DIR = os.path.join(BASE_DIR, 'profiles')
 
 
-class Register:
-    """User Registration"""
+DEV_MODE = os.environ.get(BASE_DIR, False)
 
-    def __init__(self, data, *args, **kwargs):
-        self.data = data
-        self.password_validator = PasswordValidator()
-        self.username_validator = UsernameValidator()
-        self.min_validator = MinimumLengthValidator()
-        # load password hasher class
-        hasher = getattr(importlib.import_module('hashers'), PASSWORD_HASHER)
-        self.password_hasher = hasher()
-        self.salt = secrets.token_hex(32).encode()
 
-    def fields_validation(self, *args, **kwargs):
-        username = self.data.get('username').strip()
-        self.username_validator.validate(username)
+DATABASE = {
+    'db': os.environ.get('db'),
+    'user': os.environ.get('user'),
+    'passwd': os.environ.get('passwd'),
+    'host': os.environ.get('host', 'localhost'),
+    'port': int(os.environ.get('port', 3306)),
+    'ssl': {
+        'ca': os.environ.get('ca'),
+        'cert': os.environ.get('cert'),
+        'key': os.environ.get('key'),
+    },
+    'ssl_mode': 'REQUIRED'
+}
 
-        password1 = self.data.get('password1')
-        if not password1.strip():
-            raise InputError(
-                "Password must not be empty.", 'password_empty')            
-        password2 = self.data.get('password2')
-        if password1 != password2:
-            raise InputError(
-                "The two password fields didn't match.", 'password_mismatch')
 
-        email = self.data.get('email')
-        email_confirm = self.data.get('email_confirm')
-        if email != email_confirm:
-            raise InputError(
-                'Both email addresses fields must match.', 'email_mismatch')
-        self.min_validator.validate(email, MIN_EMAIL_LENGTH)
+PASSWORD_HASHER = 'Argon2PasswordHasher'
 
-    def create(self):
-        # validate
-        self.fields_validation()
-        # password validation
-        password = self.data.get('password1')
 
-        encoded_password = self.password_hasher.encode(
-            password, self.salt.decode())
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'auth.securecodewarrior.com')
 
-        new_user = {
-            'username': self.data.get('username'),
-            'password': encoded_password,
-            'email': self.data.get('email'),
-            'date_joined': datetime.utcnow().isoformat()
-        }
 
-        return new_user
+# Symmetric Encryption and Decryption Key
 
-    def save(self):
-        user = self.create()
-        if user:
-            try:
-                conn = Connect()
-                cursor = conn.db.cursor()
-                cursor.execute(
-                    """
-                    INSERT INTO user (username, password, email, date_joined)
-                        VALUES (%(username)s, %(password)s,
-                                %(email)s, %(date_joined)s)
-                    """, user
-                )
-                conn.db.commit()
-                logger.info('User %02d inserted' % cursor.lastrowid)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-                if DEV_MODE:
-                    logger.debug(
-                        '%(username)s, %(password)s, '
-                        '%(email)s, %(date_joined)s' % user)
 
-            except MySQLdb.MySQLError as e:
-                logger.error('Could not insert provider: %s' % e)
-            finally:
-                conn.db.close()
+# Asymmetric Encryption and Decryption RSA Keys
+
+PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
+
+PUBLIC_KEY = os.environ.get('PUBLIC_KEY')
+
+
+# AWS S3 Storage
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=%s' % os.environ.get('CacheControl'),
+}
+
+AWS_LOCATION = os.environ.get('AWS_LOCATION')
