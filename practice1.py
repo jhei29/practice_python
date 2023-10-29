6a8b010f5f34a464ab65e918e6ce45e870e21c5e
Jerome <100307631+jhei29@users.noreply.github.com>
2023-10-29 15:48:46 +0800

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,90 +1,85 @@
 import logging
-from MySQLdb import MySQLError
-from database.connect import Connect
-
-from auth import Auth
-from decorators import is_authorized
-from serializers import ClientSerializer, AddressSerializer
 
 logger = logging.getLogger(__name__)
 
 
-class Client(Auth):
+class UpdateSerializer:
 
-    @is_authorized
-    def retrieve(self, uuid):
+    fields = None
 
-        result = None
+    def serialize(self, data):
 
-        conn = Connect()
-        try:
-            cursor = conn.db.cursor()
-            cursor.execute(
-                """
-                SELECT first_name, last_name, email, phone
-                FROM clients
-                WHERE uuid=UUID_TO_BIN(%(uuid)s)
-                """, {'uuid': uuid}
-            )
-            result = cursor.fetchone()
-        except MySQLError:
-            logger.error('Could not retrieve the client')
-        finally:
-            conn.db.close()
+        result = ''
+
+        keys = list(data.keys())
+        if keys:
+            key = keys[0]
+            if data.get(key):
+                result += f'{key}=%({key})s'
+            for key in keys[1:]:
+                if data.get(key):
+                    result += f', {key}=%({key})s'
+
         return result
 
-    @is_authorized
-    def update(self, uuid, **kwargs):
+    def updatefields(self, data):
 
-        result = None
-        fields = ClientSerializer().updatefields(kwargs)
-        if not fields:
-            return result
-        kwargs.update({'uuid': uuid})
+        if not (data and isinstance(data, dict)):
+            logger.error('SerializerError: data is not valid')
+            return None
 
-        conn = Connect()
-        try:
-            cursor = conn.db.cursor()
-            cursor.execute(
-                f"""
-                UPDATE clients SET {fields}
-                WHERE uuid=UUID_TO_BIN(%(uuid)s)
-                """, kwargs
-            )
-            conn.db.commit()
-            result = cursor.rowcount
+        return self.serialize(data)
 
-            if not result:
-                raise MySQLError
 
-        except MySQLError:
-            logger.error('Client could not be updated')
-        finally:
-            conn.db.close()
-        return result
+class UserUpdateSerializer(UpdateSerializer):
 
-    @is_authorized
-    def insert_address(self, **kwargs):
+    fields = ['first_name', 'last_name', 'email']
 
-        result = None
 
-        columns, values = AddressSerializer().insertfields(kwargs)
-        if not (columns and values):
-            return result
+class ClientSerializer(UpdateSerializer):
 
-        conn = Connect()
-        try:
-            cursor = conn.db.cursor()
-            cursor.execute(
-                f"""
-                INSERT INTO addresses ({columns})
-                VALUES ({values})
-                """, kwargs
-            )
-            conn.db.commit()
-            logger.info('Address %02d inserted' % cursor.lastrowid)
-        except MySQLError:
-            logger.error('Could not insert the address')
-        finally:
-            conn.db.close()
-        return result
+    fields = ['first_name', 'last_name', 'email', 'phone']
+
+
+class InsertSerializer:
+
+    fields = None
+
+    def serialize(self, data):
+
+        columns, values = '', ''
+
+        if self.fields:
+            field = self.fields[0]
+            if data.get(field):
+                columns += f'{field}'
+                values += f'%({field})s'
+            for field in self.fields[1:]:
+                if data.get(field):
+                    columns += f', {field}'
+                    values += f', %({field})s'
+
+        return columns, values
+
+    def insertfields(self, data):
+
+        if not (data and isinstance(data, dict)):
+            logger.error('SerializerError: data is not valid')
+            return None
+
+        return self.serialize(data)
+
+
+class UserInsertSerializer(InsertSerializer):
+
+    fields = ['username', 'password', 'email']
+
+
+class AppointmentSerializer(InsertSerializer):
+
+    fields = ['pet_id', 'reason', 'datetime', 'description', 'doctor_id']
+
+
+class AddressSerializer(InsertSerializer):
+
+    fields = ['line_1', 'line_2', 'city', 'state']
